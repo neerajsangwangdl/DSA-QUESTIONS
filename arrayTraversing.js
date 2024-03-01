2fa2aa5ce63a838cc482acbf37fcd95914337ad0
neerajsangwangdl <115098051+neerajsangwangdl@users.noreply.github.com>
2024-03-01 08:38:14 +0000

    array subset question done

---
+++ arrayTraversing.js
@@ -0,0 +1,16 @@
+// Neeraj 
+
+
+function getElement(){
+    let el = document.getElementById("Element").value
+    if(el<=data.length && typeof parseInt(el)==="number"){
+        alert(data[el])
+    }else{
+        alert("No")
+    }
+}
+
+
+
+//=============================================
+

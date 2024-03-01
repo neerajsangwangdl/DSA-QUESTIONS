96a10e762ceb49a376ce75e6986372d1084c8ed7
neerajsangwangdl <115098051+neerajsangwangdl@users.noreply.github.com>
2024-03-01 12:05:32 +0000

    hello

--- arrayTraversing.js
+++ arrayTraversing.js
@@ -1,21 +1,25 @@
 // Neeraj 
+// Search the element by the index no.
 
+function getElement(dataTraversing,el){
 
-// function getElement(dataTraversing,el){
+    if(el<=dataTraversing.length){
+        console.warn(dataTraversing[el]);
+    }else{
+        console.warn("The element is not in the array")
 
-//     if(el<=dataTraversing.length){
-//         console.warn(dataTraversing[el]);
-//     }else{
-//         console.warn("The element is not in the array")
+    }
+}
 
-//     }
-// }
-
-// let dataTraversing = [1,2,3,4,5,6,7,8]
-// let el = 4
-// getElement(dataTraversing,4)
+const dataTraversing = [1,2,3,4,5,6,7,8]
+const el = 4
+getElement(dataTraversing,4)
 
 console.log("Neeraj")
+
+const second = [33,44,55,66,77]
+const elb = 3
+getElement(second,elb)
 
 //=============================================
 
